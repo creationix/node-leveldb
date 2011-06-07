f538728ada69073feb251d9a44b322c32166c765
Carter Thaxton <carter.thaxton@gmail.com>
2011-06-07 02:22:17 -0700

    Updated outline of async API

--- leveldb.js
+++ leveldb.js
@@ -1,24 +1,24 @@
 // Mock implementation in JS to help guide API
 // This is meant to mirror the C++ API, but be JavaScripty
 
-function LevelDB() {}
-LevelDB.prototype.open = function open(name, options, callback) { /*...*/ };
-LevelDB.prototype.close = function close(callback) { /*...*/ };
-LevelDB.prototype.put = function put(key, value, options, callback) { /*...*/ };
-LevelDB.prototype.del = function del(key, options, callback) { /*...*/ };
-LevelDB.prototype.write = function write(updates, options, callback) { /*...*/ };
-LevelDB.prototype.get = function get(key, options, callback) { /*...*/ };
-LevelDB.prototype.newIterator = function newIterator(options) { /*...*/ };
-LevelDB.prototype.getSnapshot = function getSnapshot() { /*...*/ };
-LevelDB.prototype.releaseSnapshot = function releaseSnapshot(snapshot) { /*...*/ };
-LevelDB.prototype.getProperty = function getProperty(name) { /*...*/ };
-LevelDB.prototype.getApproximateSizes = function releaseSnapshot(ranges) { /*...*/ };
-LevelDB.openDB = function openDB(name, options, callback) { /*...*/ }; // Convience wrapper around constructor and open
-LevelDB.destroyDB = function destroyDB(name, options, callback) { /*...*/ };
-LevelDB.repairDB = function repairDB(name, options, callback) { /*...*/ };
+function DB() {}
+DB.prototype.open = function open(name, options, callback) { /*...*/ };
+DB.prototype.close = function close(callback) { /*...*/ };
+DB.prototype.put = function put(key, value, options, callback) { /*...*/ };
+DB.prototype.del = function del(key, options, callback) { /*...*/ };
+DB.prototype.write = function write(updates, options, callback) { /*...*/ };
+DB.prototype.get = function get(key, options, callback) { /*...*/ };
+DB.prototype.newIterator = function newIterator(options) { /*...*/ };
+DB.prototype.getSnapshot = function getSnapshot() { /*...*/ };
+DB.prototype.releaseSnapshot = function releaseSnapshot(snapshot) { /*...*/ };
+DB.prototype.getProperty = function getProperty(name) { /*...*/ };
+DB.prototype.getApproximateSizes = function releaseSnapshot(ranges) { /*...*/ };
+DB.openDB = function openDB(name, options, callback) { /*...*/ }; // Convience wrapper around constructor and open
+DB.destroyDB = function destroyDB(name, options, callback) { /*...*/ };
+DB.repairDB = function repairDB(name, options, callback) { /*...*/ };
 
 function WriteBatch() {}
-WriteBatch.prototype.put = function put(key, balue) { /*...*/ };
+WriteBatch.prototype.put = function put(key, value) { /*...*/ };
 WriteBatch.prototype.del = function del(key) { /*...*/ };
 WriteBatch.prototype.clear = function clear() { /*...*/ };
 
@@ -62,31 +62,30 @@ Iterator.prototype.registerCleanup = function registerCleanup(callback, arg1, ar
 
 // Sample usage ////////////////////////////////////////////////////////////////
 
-// Create a new LevelDB instance database
-var db = new LevelDB();
+// Create a new LevelDB instance
+var db = new DB();
 
 // `options` is always optional and defaults are assumed if left out
 
 // Opening a database
-db.open("/tmp/testdb", options, function (err, status) {
+db.open("/tmp/testdb", options, function (err) {
   // err will be an exception object if there is a problem
-  // otherwise status will be a string message
 });
 
 // Closing a database
-db.close(function (err, status) { /*...*/ });
+db.close(function (err) { /*...*/ });
 
 // Putting a value in the database
-db.put(key, value, options, function (err, status) { /*...*/ });
+db.put(key, value, options, function (err) { /*...*/ });
 
 // Deleting a value from the database
-db.del(key, options, function (err, status) { /*...*/ });
+db.del(key, options, function (err) { /*...*/ });
 
 // Write a batch of updates in one call
-db.write(updates, options, function (err, status) { /*...*/ });
+db.write(updates, options, function (err) { /*...*/ });
 
 // Getting a value from the database
-db.get(key, options, function (err, value, status) { /*...*/ });
+db.get(key, options, function (err, value) { /*...*/ });
 
 // get an iterator
 var iterator = db.newIterator(options);
@@ -100,5 +99,5 @@ var value = db.getProperty(name);
 
 var sized = db.getApproximateSizes(ranges);
 
-LevelDB.destroyDB(name, options, function (err, status) { /*...*/ });
-LevelDB.repairDB(name, options, function (err, status) { /*...*/ });
+DB.destroyDB(name, options, function (err) { /*...*/ });
+DB.repairDB(name, options, function (err) { /*...*/ });
